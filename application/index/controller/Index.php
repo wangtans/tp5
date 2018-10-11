54b5ee063dc5a41d7ab51d01069f025d966155f3
wangtan <1373067317@qq.com>
2018-10-11 16:03:03 +0800

    tp5

---
+++ application/index/controller/Index.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use think\Request;
+
+class Index extends Controller{
+
+    // 文件上传表单
+    public function index(){
+    return $this->fetch();
+}
+
+    // 文件上传提交
+    public function upload(){
+        // 获取表单上传文件
+        $file = request()->file('files');
+
+        if (empty($file)) {
+            $this->error('请选择上传文件');
+        }
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if ($info) {
+            $this->success('文件上传成功');
+            echo $info->getFilename();
+        } else {
+            // 上传失败获取错误信息
+            $this->error($file->getError());
+        }
+
+    }
+
+}
---
+++ application/index/controller/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use think\Request;
+
+class User extends Controller{
+
+    // 文件上传表单
+    public function index(){
+    var_dump(1);
+}
+
+}
